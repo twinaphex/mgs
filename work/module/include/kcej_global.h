cf23c855f52622b95623622ac7eed83d74462e71
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-07-29 20:41:39 -0400

    Create kcej_global.h

---
+++ work/module/include/kcej_global.h
@@ -0,0 +1,13 @@
+#ifndef INC_KCEJ_GLOBAL_H
+#define INC_KCEJ_GLOBAL_H
+
+/*----- Build Requirements -----*/
+#include "buildconf.h"
+
+/*----- Type Definitions -----*/
+#include <sys/types.h>
+#include "cmtypes_def.h"
+
+#endif // INC_KCEJ_GLOBAL_H
+
+/* vim: set noet ts=4 sw=4 ff=dos fenc=euc-jp: */
