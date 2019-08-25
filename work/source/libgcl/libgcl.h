400d8f725c60e64943fcd0f65f84990363a2e497
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-08-25 19:50:51 -0400

    Update libgcl.h

---
+++ work/source/libgcl/libgcl.h
@@ -0,0 +1,62 @@
+/*
+ * Game Command Language
+ * Script Interpreter Library
+ * for METAL GEAR SOLID
+ */
+#ifndef INC_LIBGCL_H
+#define INC_LIBGCL_H
+
+#include "mgs_global.h"
+
+/*---------------------------------------------------------------------------*
+ * GCL Structures (ref.Memory Leaks, MGS4 Symbols)
+ *---------------------------------------------------------------------------*/
+
+typedef int GCL_COMMANDFUNC(char *);
+
+typedef struct {
+	long32 id;
+	GCL_COMMANDFUNC *func;
+} GCL_COMMANDLIST;
+
+typedef struct _gcl_commanddef {
+	struct _gcl_commanddef *next;
+	long32 n_commlist;
+	GCL_COMMANDLIST *commlist;
+} GCL_COMMANDDEF;
+
+typedef struct {
+	u_short argc;
+	long32 *argv;
+} GCL_ARGS;
+
+/*---------------------------------------------------------------------------*
+ * Endianness Conversion (ref.Memory Leaks)
+ *---------------------------------------------------------------------------*/
+
+static inline long GCL_GetLong( char *ptr )
+{
+	unsigned char *p;
+	p = ( unsigned char * )ptr;
+	return ( p[ 0 ] << 24 ) | ( p[ 1 ] << 16 ) | ( p[ 2 ] << 8 ) | ( p[ 3 ] );
+}
+
+static inline long GCL_GetShort( char *ptr )
+{
+	unsigned char *p;
+	p = ( unsigned char * )ptr;
+	return ( signed short )( ( p[ 0 ] << 8 ) | ( p[ 1 ] ) );
+}
+
+static inline char GCL_GetByte( char *ptr )
+{
+	return *ptr;
+}
+
+/*---------------------------------------------------------------------------*
+ * END OF FILE
+ *---------------------------------------------------------------------------*/
+#endif // INC_LIBGCL_H
+
+/* -*- indent-tabs-mode: t; tab-width: 4; mode: c; -*- */
+/* vim: set noet ts=4 sw=4 ft=c ff=dos fenc=euc-jp : */
