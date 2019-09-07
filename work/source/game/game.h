4507cbad4670dbf759b6e24771cfdacb61161de7
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-09-07 03:17:18 -0400

    Update game.h

---
+++ work/source/game/game.h
@@ -0,0 +1,16 @@
+/*
+ * [ GAME ]
+ * Game Management Library
+ *
+ * ver.METAL GEAR SOLID
+ */
+#ifndef INC_GAME_H
+#define INC_GAME_H
+
+/* Game Daemon (gamed.c) */
+extern void GM_StartDaemon( void );
+
+#endif /* INC_GAME_H */
+
+/* -*- indent-tabs-mode: t; tab-width: 4; mode: c; -*- */
+/* vim: set noet ts=4 sw=4 ft=c ff=dos fenc=euc-jp : */
