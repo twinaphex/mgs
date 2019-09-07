52d72e1e05b5e68567a29ad3955a27a5e38a25d4
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-09-07 03:18:13 -0400

    Update libhzd.h

---
+++ work/source/libhzd/libhzd.h
@@ -0,0 +1,16 @@
+/*
+ * [ LibHZD ]
+ * Collision Library
+ *
+ * ver.METAL GEAR SOLID
+ */
+#ifndef INC_LIBHZD_H
+#define INC_LIBHZD_H
+
+/* HZD Daemon (hzdd.c) */
+extern void HZD_StartDaemon( void );
+
+#endif /* INC_LIBHZD_H */
+
+/* -*- indent-tabs-mode: t; tab-width: 4; mode: c; -*- */
+/* vim: set noet ts=4 sw=4 ft=c ff=dos fenc=euc-jp : */
