11e55e54f3802aeb4015d11a9e24cf599a50f2cc
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-09-07 03:17:40 -0400

    Update libgv.h

--- work/source/libgv/libgv.h
+++ work/source/libgv/libgv.h
@@ -1,12 +1,26 @@
+/*
+ * [ LibGV ]
+ * Memory Management,
+ * Execution Unit Control,
+ * & Common Math Library
+ *
+ * ver.METAL GEAR SOLID
+ */
 #ifndef INC_LIBGV_H
 #define INC_LIBGV_H
 
 #include "mgs_global.h"
 
+/* GV Daemon (gvd.c) */
+extern void GV_StartDaemon( void );
+
+/* Actor Execution System (actor.c) */
+extern void GV_ExecActorSystem( void );
+
 /* String Hash Function (strcode.c) */
 extern u_short GV_StrCode( char *string );
 
-#endif // INC_LIBGV_H
+#endif /* INC_LIBGV_H */
 
 /* -*- indent-tabs-mode: t; tab-width: 4; mode: c; -*- */
 /* vim: set noet ts=4 sw=4 ft=c ff=dos fenc=euc-jp : */
