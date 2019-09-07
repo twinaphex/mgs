54f214c7ab4e5fb12caa929c564268e0519ef785
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-09-07 03:13:16 -0400

    update KCEJ includes

--- work/module/include/kcej_defs.h
+++ work/module/include/kcej_defs.h
@@ -1,9 +1,13 @@
+/*
+ * KCEJ West Libraries
+ * Common Definitions
+ */
 #ifndef INC_KCEJ_DEFS_H
 #define INC_KCEJ_DEFS_H
 
 
 
-#endif // INC_KCEJ_DEFS_H
+#endif /* INC_KCEJ_DEFS_H */
 
 /* -*- indent-tabs-mode: t; tab-width: 4; mode: c; -*- */
 /* vim: set noet ts=4 sw=4 ft=c ff=dos fenc=euc-jp : */
--- work/module/include/kcej_global.h
+++ work/module/include/kcej_global.h
@@ -1,14 +1,18 @@
+/*
+ * KCEJ West Libraries
+ * Global Includes
+ */
 #ifndef INC_KCEJ_GLOBAL_H
 #define INC_KCEJ_GLOBAL_H
 
-/*----- Build Requirements -----*/
+/* Project Config */
 #include "buildconf.h"
 
-/*----- KCEJ Definitions -----*/
+/* KCEJ Includes */
 #include "kcej_defs.h"
 #include "kcej_types.h"
 
-#endif // INC_KCEJ_GLOBAL_H
+#endif /* INC_KCEJ_GLOBAL_H */
 
 /* -*- indent-tabs-mode: t; tab-width: 4; mode: c; -*- */
 /* vim: set noet ts=4 sw=4 ft=c ff=dos fenc=euc-jp : */
