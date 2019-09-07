8208cfb472044f8aec3a95e1f6019b5c8e29fb58
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-09-07 03:21:09 -0400

    add isio.h

---
+++ work/module/libsio/isio.h
@@ -0,0 +1,27 @@
+/*
+ * Internal SIO Driver for PSX
+ * (interrupt version)
+ *
+ * ref. : work.5th/include/isio.h
+ */
+#ifndef __ISIO_H__
+#define __ISIO_H__
+
+#ifndef __ISIO_C__
+#define EXTERN  extern
+#else
+#define EXTERN
+#endif
+
+EXTERN void          sio_putchar( unsigned char );
+EXTERN unsigned char sio_getchar( void );
+EXTERN int           sio_getchar2( void );
+EXTERN int           sio_chkbuf( void );
+EXTERN void          sio_flush( void );
+EXTERN void          sio_puts( unsigned char * );
+EXTERN int           sio_setup( int );
+EXTERN void          sio_dump( unsigned char *, unsigned long );
+EXTERN int           sio_get_dsr( void );
+EXTERN int           sio_get_cts( void );
+
+#endif /* __ISIO_H__ */
