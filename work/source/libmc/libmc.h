102fe8fdce080cac41237cf7f55137d41e73f28a
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-09-07 03:17:00 -0400

    update libmc

---
+++ work/source/libmc/libmc.h
@@ -0,0 +1,16 @@
+/*
+ * [ LibMC ]
+ * Memory Card Library for PSX
+ *
+ * ver.METAL GEAR SOLID
+ */
+#ifndef INC_LIBMC_H
+#define INC_LIBMC_H
+
+/* MC Daemon */
+extern void MC_StartDaemon( void );
+
+#endif /* INC_LIBMC_H */
+
+/* -*- indent-tabs-mode: t; tab-width: 4; mode: c; -*- */
+/* vim: set noet ts=4 sw=4 ft=c ff=dos fenc=euc-jp : */
