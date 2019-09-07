fdc2a83b50efccdf874e9d82e173104bcdf4f3db
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-09-07 03:19:57 -0400

    add mgs_libs.h

---
+++ work/source/include/mgs_libs.h
@@ -0,0 +1,29 @@
+/*
+ * METAL GEAR SOLID
+ * Library Inlcudes
+ */
+#ifndef INC_MGS_LIBS_H
+#define INC_MGS_LIBS_H
+
+/* MODULE */
+#include "mts.h"
+#include "taskid.h"
+#include "isio.h"
+#include "sound.h"
+
+/* SYSTEM */
+#include "libdg.h"
+#include "libfs.h"
+#include "libgcl.h"
+#include "libgv.h"
+#include "libhzd.h"
+#include "libmc.h"
+#include "font.h"
+
+/* GAME */
+#include "game.h"
+
+#endif /* INC_MGS_LIBS_H */
+
+/* -*- indent-tabs-mode: t; tab-width: 4; mode: c; -*- */
+/* vim: set noet ts=4 sw=4 ft=c ff=dos fenc=euc-jp : */
