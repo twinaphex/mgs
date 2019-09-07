e3390e185e89cf003f233e6e9619efd9279ae4fb
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-09-07 03:19:45 -0400

    add sound.h

---
+++ work/module/include/sound.h
@@ -0,0 +1,16 @@
+/*
+ * Sound Driver for PSX
+ * Public Interface
+ * 
+ * ver.METAL GEAR SOLID
+ */
+#ifndef INC_SOUND_H
+#define INC_SOUND_H
+
+extern void SdMain( void );
+extern int  SdIntReady( void );
+
+#endif /* INC_SOUND_H */
+
+/* -*- indent-tabs-mode: t; tab-width: 4; mode: c; -*- */
+/* vim: set noet ts=4 sw=4 ft=c ff=dos fenc=euc-jp : */
