be4fa017a776e59300d479b6a382dd7688d10564
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-09-07 03:17:23 -0400

    Update libdg.h

---
+++ work/source/libdg/libdg.h
@@ -0,0 +1,16 @@
+/*
+ * [ LibDG ]
+ * Image-Drawing & Display Library
+ *
+ * ver.METAL GEAR SOLID
+ */
+#ifndef INC_LIBDG_H
+#define INC_LIBDG_H
+
+/* DG Daemon (dgd.c) */
+extern void DG_StartDaemon( void );
+
+#endif /* INC_LIBDG_H */
+
+/* -*- indent-tabs-mode: t; tab-width: 4; mode: c; -*- */
+/* vim: set noet ts=4 sw=4 ft=c ff=dos fenc=euc-jp : */
